0e5d2f7926f219c554afbf03838b8f75d4f0e521
vkdatta <134008395+vkdatta@users.noreply.github.com>
2025-07-15 18:44:46 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,3 @@
 from setuptools import setup
 
-setup(
-  name='termuxessentialsbyvk',
-  version='0.0',
-  description='A lightweight Termux toolkit to copy, erase, overwrite file content and scrape web tables/links with a single command.',
-  author='vkd',
-    scripts=['script/copy','script/erase','script/overwrite','script/xtract'],
-  python_requires='>=3.6',
-  classifiers=[
-    'Programming Language :: Python :: 3',
-    'License :: OSI Approved :: MIT License',
-    'Operating System :: OS Independent',
-  ],
-)
+setup()
